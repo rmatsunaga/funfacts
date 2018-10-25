d6cf388b4c40e294faa55f7e61e76fd398d723d5
rmatsunaga <matsunagar97@gmail.com>
2018-10-25 12:56:15 -0700

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Rey Matsunaga on 10/25/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
