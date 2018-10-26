ea2f911dcaa5b735e3f9c25a0fadb3da094f70c1
Rey <matsunagar97@gmail.com>
2018-10-25 21:05:10 -0700

    Built random fact generator

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    @IBOutlet weak var funFactButton: UIButton!
+    let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factProvider.randomFact()     }
+
+
+    @IBAction func showFact() {
+        
+        funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
     }
-
-
 }
+
 
