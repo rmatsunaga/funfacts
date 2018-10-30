2aa8d3f884c7cac14f0020cff3ae3630bfe5a415
Rey <matsunagar97@gmail.com>
2018-10-30 16:54:17 -0700

    Govardhan helped show me storyboarding and introduced me to viewcontroller lifecycle

---
+++ FunFacts/AlarmViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmViewController.swift
+//  FunFacts
+//
+//  Created by Rey Matsunaga on 10/30/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlarmViewController: UIViewController {
+    
+    
+    
+}
